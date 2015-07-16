81e699f1edcd09a467c4a2ca7d1f25f308c5d433
Ubuntu <ubuntu@ip-172-31-19-223.us-west-2.compute.internal>
2015-07-16 19:27:49 +0000

    add runner script

---
+++ run.sh
@@ -0,0 +1,16 @@
+NP=$1
+if [ -z $NP ]
+then
+	echo "Missing number of processes, usage: $0 <NP> <SYNC[1|2|3]>"
+	exit 1
+fi
+
+SYNC=$2
+if [ -z $SYNC ]
+then
+	echo "Missing sync type, usage: $0 <NP> <SYNC[0|1|2]>"
+	exit 1
+fi
+echo $SYNC
+
+mpirun -np $NP ./Burst_Buffer --sync=$SYNC --codes-config=pvfs_simplenet.conf
