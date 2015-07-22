dec66817e24c918da1bbea6f92ffc6910bbee057
tforlini <tforlini@hawk.it.edu>
2015-07-21 21:21:08 -0500

    test is_in_svr

--- Burst_Buffers.c
+++ Burst_Buffers.c
@@ -104,7 +104,8 @@ void node_lp_init(node_state * ns, tw_lp * lp){
 
     // track which cluster we're in
     ns->is_in_client = (id_all < num_client_nodes);
-    //ns->is_in_server = (id_all < (num_svr_nodes + num_client_nodes) && (id_all >= num_client_nodes));
+    ns->is_in_server = (id_all < (num_svr_nodes + num_client_nodes) && (id_all >= num_client_nodes));
+
     printf("id_all= %d\nnum_client_nodes= %d\n",id_all,num_client_nodes);
     // send a self kickoff event
     tw_event *e = codes_event_new(lp->gid, codes_local_latency(lp), lp);
