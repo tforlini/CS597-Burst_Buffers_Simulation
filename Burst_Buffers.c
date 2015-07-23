3b9eb91f48aa53306aa73e06a107deb6f7b66845
tforlini <tforlini@hawk.it.edu>
2015-07-22 21:43:20 -0500

    remove printf

--- Burst_Buffers.c
+++ Burst_Buffers.c
@@ -396,7 +396,6 @@ void handle_forwarder_recv(forwarder_state * ns,forwarder_msg * m,tw_lp * lp) {
     		net_id=net_id_svr;
     	}
     	else if(m->node_event_type == NODE_RECV_ack){
-    		printf("Test LOLOLOLO");
     		dest_group = "svr_CLUSTER";
     		annotation = "svr";
     		category = "ack";
