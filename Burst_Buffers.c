f22339239b1db92a6ace31ff2a7e4b8b1a536ab2
tforlini <tforlini@hawk.it.edu>
2015-07-27 11:47:15 -0500

    test io noise

--- Burst_Buffers.c
+++ Burst_Buffers.c
@@ -143,7 +143,7 @@ void node_finalize(node_state * ns,tw_lp * lp){
     }
 
     //float io_noise = 0.05 * tw_rand_integer(lp->rng,ns->pvfs_ts_remote_write,ns->pvfs_ts_remote_write);
-    float io_noise = ns->pvfs_ts_remote_write * tw_rand_integer(lp->rng,0,0.05);
+    float io_noise = ns->pvfs_ts_remote_write * tw_rand_integer(lp->rng,0,1);
     printf("--------	IO Noise : %f	------\n",io_noise);
     float io_noise_bb = 0.05* tw_rand_integer(lp->rng,ns->bb_ts_remote_write,ns->bb_ts_remote_write);
 
