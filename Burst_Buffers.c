5f56b4e08a4d6e29855ce6f688e35cbf3caa1899
tforlini <tforlini@hawk.it.edu>
2015-08-10 10:49:43 -0500

    debug

--- Burst_Buffers.c
+++ Burst_Buffers.c
@@ -179,7 +179,7 @@ void storage_node_send_ack(node_state * ns,node_msg * m,tw_lp * lp){
 		m_fwd.node_event_type = NODE_RECV_ack;
 
 		// compute the dest forwarder index, again using a simple modulus
-		int dest_fwd_id = ns->id_clust % num_burst_buffer_forwarders;
+		int dest_fwd_id = ns->id_clust % num_storage_forwarders;
 
 		// as the relative forwarder IDs are with respect to groups, the group
 		// name must be used
